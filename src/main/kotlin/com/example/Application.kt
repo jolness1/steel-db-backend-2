9794f690b3ac5c530f0b56de65fa7f6aebe34021
Jacob Olness <jolness1@gmail.com>
2023-11-01 16:32:18 -0600

    oops, set up to use routing plugin now

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,15 +1,11 @@
 package com.example
 
+import com.example.plugins.configureRouting
 import io.ktor.serialization.kotlinx.json.json
-import io.ktor.server.application.Application
-import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.server.response.respond
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
 import kotlinx.serialization.json.Json
 
 
@@ -17,21 +13,10 @@ fun main() {
     embeddedServer(Netty, port = 8080) {
         configureRouting()
         install(ContentNegotiation) {
-            json(
-                Json {
-                    prettyPrint = true
-                    isLenient = true
-                }
-            )
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            })
         }
     }.start(wait = true)
 }
-
-fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respond("hello world")
-        }
-    }
-}
-
